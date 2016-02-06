9ecd9bf9dd3f74422e0c937dd60892a5be67c443
Marcio Junior <marciojunior_eu@yahoo.com.br>
2016-02-06 14:06:05 -0200

    Annotate classes passed as argument

---
+++ tests/fixtures/classes_as_params/actual.js
@@ -0,0 +1,7 @@
+angular
+  .module('app.reports')
+  .controller('ReportsController', class ReportsController {
+    constructor(data) {
+      this.data = data;
+    }
+  });
---
+++ tests/fixtures/classes_as_params/expected.js
@@ -0,0 +1,7 @@
+angular
+  .module('app.reports')
+  .controller('ReportsController', ['data', class ReportsController {
+    constructor(data) {
+      this.data = data;
+    }
+  }]);
